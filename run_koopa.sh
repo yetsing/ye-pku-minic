581a45618998e678669835da859b3173b6a82f4b
yetsing <heopro@163.com>
2025-03-17 20:08:31 +0800

    添加调试日志选项以便于调试

--- run_koopa.sh
+++ run_koopa.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-cmake -S "/root/compiler" -B "/tmp/debug" -DLIB_DIR="/opt/lib/native" -DINC_DIR="/opt/include"
+debug="${2:-false}"
+if [ "$debug" = "false" ]; then
+    cmake -S "/root/compiler" -B "/tmp/debug" -DLIB_DIR="/opt/lib/native" -DINC_DIR="/opt/include" -DENABLE_DEBUG_LOG=OFF
+else
+    cmake -S "/root/compiler" -B "/tmp/debug" -DLIB_DIR="/opt/lib/native" -DINC_DIR="/opt/include" -DENABLE_DEBUG_LOG=ON
+fi
+
+# cmake -S "/root/compiler" -B "/tmp/debug" -DLIB_DIR="/opt/lib/native" -DINC_DIR="/opt/include"
 cmake --build "/tmp/debug" --target compiler
 echo "Compilation successful: /tmp/debug/compiler"
 
